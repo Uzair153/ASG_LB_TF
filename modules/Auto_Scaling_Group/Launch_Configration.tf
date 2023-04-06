16ddbf605ef42af27e430c9695aa8285776fb4b4
Uzair Ahmad <uzairsheikh153@gmail.com>
2023-04-06 13:11:54 -0700

    update code

--- modules/Auto_Scaling_Group/Launch_Configration.tf
+++ modules/Auto_Scaling_Group/Launch_Configration.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "LC" {
   image_id        = var.ami_id
   instance_type   = var.instance_type
   security_groups = [aws_security_group.SG.id]
-  #  vpc_security_group_ids = [aws_security_group.SG.id]
+  associate_public_ip_address = true
   key_name        = aws_key_pair.test-key.key_name
   
 }
